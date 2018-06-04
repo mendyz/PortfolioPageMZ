50d4c790b1c3c014e78a4f8b8517c48b8aedac22
MendyZ <exclusivecanvasart@gmail.com>
2018-06-04 13:43:49 -0500

    Added CodePen Portfolio Page
    Added CodePen Portfolio Page from Desktop Github App

---
+++ js/index.js
@@ -0,0 +1,7 @@
+ //$("#starynight").addClass("animated zoomInDown");
+ //$(".pictures").addClass("animated zoomInDown");
+//$("#van-gogh-title").addClass("animated fadeIn")
+//$(".rounded-container").addClass("animated pulse")
+$("#old-buttons").hide()
+$(".pictures").hide()
+$(".nav-old").hide()
